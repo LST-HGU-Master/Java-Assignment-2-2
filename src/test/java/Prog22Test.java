157043df698bda4d4d8ea9bf8604b87f8280a1c1
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-04-18 21:53:52 +0900

    changed numbers

--- src/test/java/Prog22Test.java
+++ src/test/java/Prog22Test.java
@@ -1,29 +1,72 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import java.io.*;
 
 public class Prog22Test {
-
-   @Test
-   public void testStandardInputs()
-   {
-       PrintStream originalOut = System.out;
-       ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    InputStream originalIn;
+    PrintStream originalOut;
+    ByteArrayOutputStream bos;
+    StandardInputStream in;
+    
+    @BeforeEach
+    void before() {
+       //back up binding
+       originalIn  = System.in;
+       originalOut = System.out;
+       //modify binding
+       bos = new ByteArrayOutputStream();
        System.setOut(new PrintStream(bos));
-
-       StandardInputStream in = new StandardInputStream();
+        
+       in = new StandardInputStream();
        System.setIn(in);
+    }
+    
+    @AfterEach
+    void after() {
+       System.setOut(originalOut);
+       // System.setIn(new BufferedInputStream(new FileInputStream(FileDescriptor.in)));
+       System.setIn(originalIn);
+    }
 
+    @Test
+    public void testFirstSentence()
+    {
        // action
-       in.inputln("1309");
+       in.inputln("3"); // any string is OK
        Prog22.main(null);
 
        // assertion
-       String[] prints = bos.toString().split("\n");
-       assertEquals("1309を2で割った余りは1です", prints[prints.length - 1]);
+       String[] prints = bos.toString().split(System.lineSeparator());
+       try {
+            assertEquals("整数を入力してください", prints[0]);
+       }catch (AssertionError e){
+            after();
+            throw e;
+       }
+    }
 
-       // undo the binding in System
-       System.setOut(originalOut);
-       System.setIn(new BufferedInputStream(new FileInputStream(FileDescriptor.in)));
-   }
+    @Test
+    public void testSecondSentence()
+    {
+       // action
+       String kbIn = "5";
+       int modu = 5;
+       in.inputln(kbIn);
+       Prog22.main(null);
+
+       // assertion
+       String[] prints = bos.toString().split(System.lineSeparator());
+       assertEquals(kbIn + "を" + modu +"で割った余りは" + (Integer.parseInt(kbIn) % modu) +"です", prints[1]);
+
+       // if (prints.length == 2) {
+           // assertEquals(kbIn + "を2で割った余りは" + (Integer.parseInt(kbIn) % 2) +"です", prints[1]);
+       // }
+       // else { // as no LF expected, test second printed line ... further consideration required 
+           // String afterPromptWord = prints[0].substring(11);
+           // assertEquals(kbIn + "を2で割った余りは" + (Integer.parseInt(kbIn) % 2) +"です",afterPromptWord);
+       // }
+
+    }
 }
